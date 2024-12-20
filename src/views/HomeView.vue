efccfb5347d0c6179240990213d2025a2b2f494b
higashiyama <higashiyama2024fcc@gmail.com>
2024-12-21 00:21:31 +0900

    komitto5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Login from '../components/Login.vue'
+import UserList from '../components/UserList.vue'
 </script>
 
 <template>
   <main>
     <Login />
+    <UserList />
   </main>
 </template>
